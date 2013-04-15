383d9d87c5942226d8c9aadfccc110e3c10ce250
Sebastien Letelie <sebastien.letelie@gmail.com>
2013-04-15 23:08:18 +0200

    refactor2

--- public.sebmade/app/scripts/controllers/player.js
+++ public.sebmade/app/scripts/controllers/player.js
@@ -4,31 +4,33 @@ angular.module('public.sebmadeApp')
     .controller('PlayerCtrl', function ($scope, $http, $routeParams, $log, $location) {
         $scope.player = {};
 
-        $http.get("/players/" + $routeParams.id)
-            .success(function (data) {
-                $scope.player = data;
-            })
-            .error(function () {
-                $log.error("player get fail")
-            });
+        if ($routeParams.id) {
+            $http.get("/players/" + $routeParams.id)
+                .success(function (data) {
+                    $scope.player = data;
+                })
+                .error(function () {
+                    $log.error("player get fail")
+                });
+        }
 
         $scope.updatePlayer = function () {
-            if ($scope.player.id) {
-                $http.put("/players/" + $scope.player.id, $scope.player)
-                    .success(function (data) {
-                        $log.info("update player : " + data.id);
-                        $location.path("#/");
-                    }).error(function () {
-                        $log.error("player update fail")
-                    });
-            } else {
-                $http.post("/players", $scope.player)
-                    .success(function (data) {
-                        $log.info("add player : " + data);
-                        $location.path("#/");
-                    }).error(function () {
-                        $log.error("player add fail")
-                    });
-            }
+            $http.put("/players/" + $scope.player.id, $scope.player)
+                .success(function (data) {
+                    $log.info("update player : " + data.id);
+                    $location.path("#/");
+                }).error(function () {
+                    $log.error("player update fail")
+                });
+        };
+
+        $scope.addPlayer = function () {
+            $http.post("/players", $scope.player)
+                .success(function (data) {
+                    $log.info("add player : " + data);
+                    $location.path("#/");
+                }).error(function () {
+                    $log.error("player add fail")
+                });
         };
     });
