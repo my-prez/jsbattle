726706464f907ce94b78a4e14816d8ad1f54e6a5
tchak <paul@chavard.net>
2013-04-28 14:08:09 +0200

    cleanup adapter

--- public.tchak/app/store.js
+++ public.tchak/app/store.js
@@ -2,43 +2,41 @@ App.Adapter = DS.RESTAdapter.extend({
   serializer: DS.JSONSerializer,
 
   didFindRecord: function(store, type, json, id) {
-    var root = this.rootForType(type);
-    var data = {};
-    data[root] = json;
-    this._super(store, type, data, id);
+    json = this.extractData(type, json);
+    this._super(store, type, json, id);
   },
   didFindAll: function(store, type, json) {
-    var root = this.pluralize(this.rootForType(type));
-    var data = {};
-    data[root] = json;
-    this._super(store, type, data);
+    json = this.extractData(type, json, true);
+    this._super(store, type, json);
   },
   didFindQuery: function(store, type, json, array) {
-    var root = this.pluralize(this.rootForType(type));
-    var data = {};
-    data[root] = json;
-    this._super(store, type, data, array);
+    json = this.extractData(type, json, true);
+    this._super(store, type, json, array);
   },
 
   didCreateRecord: function(store, type, record, json) {
-    var root = this.rootForType(type);
-    var data = {};
-    data[root] = json;
-    this._super(store, type, record, data);
+    json = this.extractData(type, json);
+    this._super(store, type, record, json);
   },
-
   didUpdateRecord: function(store, type, record) {
     this._super(store, type, record);
   },
-
   didDeleteRecord: function(store, type, record) {
     this._super(store, type, record);
   },
 
   ajax: function(url, method, hash) {
     if (hash.data && hash.data.player) { hash.data = hash.data.player; }
-    if (hash.data && hash.data.fight) { hash.data = hash.data.fight; }
+    else if (hash.data && hash.data.fight) { hash.data = hash.data.fight; }
     this._super(url, method, hash);
+  },
+
+  extractData: function(type, json, plural) {
+    var root = this.rootForType(type);
+    var data = {};
+    root = plural ? this.pluralize(root) : root;
+    data[root] = json;
+    return data;
   }
 });
 
